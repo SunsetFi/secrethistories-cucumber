8a72e231441e7525f303a96ef5a6f5b1db8a4697
SunsetFi <sunsetfidev@gmail.com>
2023-08-26 09:40:08 -0700

    missing commit

--- src/steps/givens/situations.ts
+++ src/steps/givens/situations.ts
@@ -3,40 +3,6 @@ import HttpStatusCodes from "http-status-codes";
 
 import { api, throwForStatus } from "../../api.js";
 
-Given(/^I have a(?:n?) (\S+) verb on the tabletop$/, async (verbId: string) => {
-  try {
-    await api.createSituationAtPath("~/tabletop", {
-      verbId,
-    });
-  } catch (err: any) {
-    throwForStatus(err, {
-      [HttpStatusCodes.NOT_FOUND]: `Could not find the tabletop sphere.`,
-      [HttpStatusCodes.BAD_REQUEST]: `Cannot create a situation from verb id ${verbId} as the verb does not exist.`,
-    });
-
-    throw err;
-  }
-});
-
-Given(
-  /^I have a(?:n?) (\S+) verb on the tabletop with recipe (\S+)$/,
-  async (verbId: string, recipeId: string) => {
-    try {
-      await api.createSituationAtPath("~/tabletop", {
-        verbId,
-        recipeId,
-      });
-    } catch (err: any) {
-      throwForStatus(err, {
-        [HttpStatusCodes.NOT_FOUND]: `Could not find the tabletop sphere.`,
-        [HttpStatusCodes.BAD_REQUEST]: `Cannot create a situation from verb id ${verbId} as the verb does not exist.`,
-      });
-
-      throw err;
-    }
-  }
-);
-
 Given(
   /^I have a(?:n?) (\S+) verb in the (\S+) sphere$/,
   async (verbId: string, spherePath: string) => {
